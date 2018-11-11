f015ccc6678928b3a659188d9959dd958c78cde2
Kofanskih <kofanskih08@gmail.com>
2018-11-11 13:09:50 +0200

    commit3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("me");
         System.out.println("you");
+        System.out.println("sun");
 	// write your code here
     }
 }
